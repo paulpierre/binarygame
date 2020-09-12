62f695bda677500ec68688cc35f1e9a886ee5e16
Paul Pierre <guitarsmith@gmail.com>
2020-09-12 13:39:12 -0400

    Update config.py

--- app/config.py
+++ app/config.py
@@ -14,14 +14,14 @@ connex_app = connexion.App(__name__, specification_dir=basedir)
 app = connex_app.app
 
 # Build the Sqlite ULR for SqlAlchemy
-mysql_url = "mysql+pymysql://root:00g4B00g4!@localhost/artemis"
+mysql_url = "mysql+pymysql://root:******@localhost/artemis"
 
 # Configure the SqlAlchemy part of the app instance
 app.config["SQLALCHEMY_ECHO"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = mysql_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-app.config["SECRET_KEY"] = "F8E41A071F501D0A193B7AAC866BE72331BDB039" #Sha1 00g4B00g4!2019
-app.config["SECURITY_PASSWORD_SALT"] = "DD2EDB87EA9EB7A32FD4057276D3A1FAB861C1D5" #Sha1 fuckyou
+app.config["SECRET_KEY"] = "F8E41A071F501D0A193B7AAC866BE72331BDB03"
+app.config["SECURITY_PASSWORD_SALT"] = "DD2EDB87EA9EB7A32FD40572763A1FAB861C1D"
 
 # Create the SqlAlchemy db instance
 db = SQLAlchemy(app)
